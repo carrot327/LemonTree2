bbda9340e822ff724558a22bb810f4a4050f08cf
karl <1208484060@qq.com>
2020-02-23 17:14:15 +0800

    更新人脸sdk（添加新版的sdk）

---
+++ DFLivenessLibrary/src/main/java/com/liveness/dflivenesslibrary/view/ITimeViewBase.java
@@ -0,0 +1,8 @@
+package com.liveness.dflivenesslibrary.view;
+
+public interface ITimeViewBase {
+	public void setProgress(float currentTime);
+	public void hide();
+	public void show();
+	public int getMaxTime();
+}
